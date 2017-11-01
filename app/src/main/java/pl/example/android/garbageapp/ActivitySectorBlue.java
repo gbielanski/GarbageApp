8fe1d92d73621da2cfaa3567bca478135dc0d587
gbielanski <gbielanski@poczta.onet.pl>
2017-11-01 20:29:04 +0100

    recycler view with data binding for blue sector added

--- app/src/main/java/pl/example/android/garbageapp/ActivitySectorBlue.java
+++ app/src/main/java/pl/example/android/garbageapp/ActivitySectorBlue.java
@@ -1,16 +1,27 @@
 package pl.example.android.garbageapp;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
 import android.widget.Toast;
+
+import pl.example.android.garbageapp.databinding.ActivitySectorBlueBinding;
+
+import static android.support.v7.widget.LinearLayoutManager.VERTICAL;
 
 public class ActivitySectorBlue extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sector_blue);
+        ActivitySectorBlueBinding binding  =  DataBindingUtil.setContentView(this, R.layout.activity_sector_blue);
+        SectorTermsAdapter sectorTermsAdapter = new SectorTermsAdapter();
+        LinearLayoutManager verticalLinearLayoutManager = new LinearLayoutManager(this, VERTICAL, false);
+        binding.rcSectorTerms.setLayoutManager(verticalLinearLayoutManager);
+        binding.rcSectorTerms.setAdapter(sectorTermsAdapter);
+
     }
 
     public void showToast(View view) {
