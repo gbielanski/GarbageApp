7b310ccdeecc7cd05008990b5007ee588a9d52d3
gbielanski <gbielanski@poczta.onet.pl>
2017-11-03 16:51:37 +0100

    SectorTermDao added.

--- app/src/main/java/pl/example/android/garbageapp/data/database/SectorTerm.java
+++ app/src/main/java/pl/example/android/garbageapp/data/database/SectorTerm.java
@@ -5,22 +5,44 @@ import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.Index;
 import android.arch.persistence.room.PrimaryKey;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 @Entity(tableName = "sector_terms", indices = {@Index(value = {"sectorType"})})
 public class SectorTerm {
+    private static final  int greenSectorType = 1;
+    private static final  int blueSectorType = 2;
+    private static final  int yellowSectorType = 3;
+
+    public static final int termTypeMixed = 1;
+    public static final int termTypeSegregated = 2;
+
     @PrimaryKey(autoGenerate = true)
     private int id;
-    private String term;
-    private String termType;
-    private String sectorType;
+    private Date term;
+    private int termType;
+    private int sectorType;
 
     @Ignore
-    public SectorTerm(String term, String termType) {
-        this.term = term;
+    public SectorTerm(String term, int termType) {
+        String dateFormat = "yyyy.MM.dd";
+        Date dateTerm = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat);
+        try {
+            dateTerm = simpleDateFormat.parse(term);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        this.term = dateTerm;
         this.termType = termType;
+        sectorType = greenSectorType;
     }
 
     // Constructor used by Room
-    public SectorTerm(int id, String term, String termType, String sectorType) {
+    public SectorTerm(int id, Date term, int termType, int sectorType) {
         this.id = id;
         this.term = term;
         this.termType = termType;
@@ -28,11 +50,38 @@ public class SectorTerm {
     }
 
     public String getTerm() {
-        return term;
+        String dateFormat = "EEEE, d MMMM YYYY";
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat(dateFormat, new Locale("pl", "PL"));
+        return simpleDateFormat.format(term);
     }
+
+    //TODO extract outside
     public String getTermType() {
-        return termType;
+        switch (termType) {
+            case termTypeMixed:
+                return "zmieszane";
+            case termTypeSegregated:
+                return "segregowane";
+            default:
+                return "UNKNOWN";
+        }
     }
-    public int getId() { return id; }
-    public String getSectorType() { return sectorType; }
+
+    public int getId() {
+        return id;
+    }
+
+    //TODO extract outside
+    public String getSectorType() {
+        switch (sectorType) {
+            case greenSectorType:
+                return "GREEN";
+            case blueSectorType:
+                return "BLUE";
+            case yellowSectorType:
+                return "YELLOW";
+            default:
+                return "UNKNOWN";
+        }
+    }
 }
