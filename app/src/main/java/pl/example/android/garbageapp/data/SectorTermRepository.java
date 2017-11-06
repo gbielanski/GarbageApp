9d18999319d7be1ae5fb363e38f2cbdad807434d
Tomasz Milczarek <tomasz.milczarek@gmail.com>
2017-11-06 01:27:29 +0100

    18 Add repository & and intent service class; Implement IntentService class

---
+++ app/src/main/java/pl/example/android/garbageapp/data/SectorTermRepository.java
@@ -0,0 +1,8 @@
+package pl.example.android.garbageapp.data;
+
+/**
+ * Created by miltomasz on 03/11/17.
+ */
+
+public class SectorTermRepository {
+}
---
+++ app/src/main/java/pl/example/android/garbageapp/utils/InjectorUtils.java
@@ -0,0 +1,15 @@
+package pl.example.android.garbageapp.utils;
+
+import android.content.Context;
+
+import pl.example.android.garbageapp.data.network.SectorTermsNetworkDataSource;
+
+/**
+ * Created by miltomasz on 06/11/17.
+ */
+
+public class InjectorUtils {
+    public static SectorTermsNetworkDataSource provideNetworkDataSource(Context applicationContext) {
+        throw new UnsupportedOperationException("Not implemented yet!");
+    }
+}
