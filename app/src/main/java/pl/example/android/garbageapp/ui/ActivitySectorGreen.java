b4ab1543902a49b0c76057ddd7e7340f56f911d3
gbielanski <gbielanski@poczta.onet.pl>
2017-11-11 21:19:11 +0100

    BaseActivitySector added

--- app/src/main/java/pl/example/android/garbageapp/ui/ActivitySectorGreen.java
+++ app/src/main/java/pl/example/android/garbageapp/ui/ActivitySectorGreen.java
@@ -2,6 +2,7 @@ package pl.example.android.garbageapp.ui;
 
 import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -15,9 +16,17 @@ import pl.example.android.garbageapp.utilities.InjectorUtils;
 
 import static android.support.v7.widget.LinearLayoutManager.VERTICAL;
 
-public class ActivitySectorGreen extends AppCompatActivity {
+public class ActivitySectorGreen extends BaseActivitySector {
 
-    private DetailActivityViewModel mViewModel;
+    @Override
+    protected FragmentActivity currentSector() {
+        return this;
+    }
+
+    @Override
+    protected SectorType sectorType() {
+        return SectorType.GREEN;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,9 +36,6 @@ public class ActivitySectorGreen extends AppCompatActivity {
         LinearLayoutManager verticalLinearLayoutManager = new LinearLayoutManager(this, VERTICAL, false);
         binding.rcSectorTerms.setLayoutManager(verticalLinearLayoutManager);
         binding.rcSectorTerms.setAdapter(sectorTermsAdapter);
-
-        DetailViewModelFactory factory = InjectorUtils.provideDetailViewModelFactory(this.getApplicationContext(), SectorType.GREEN);
-        mViewModel = ViewModelProviders.of(this, factory).get(DetailActivityViewModel.class);
 
         //TODO mViewModel observe and bind data to UI
     }
