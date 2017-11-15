48582be22839d9ef18e4e3163e6fabe3fc2049ac
Tomasz Milczarek <tomasz.milczarek@gmail.com>
2017-11-15 18:50:12 +0100

    25-create-job-service-for-notifications Add NotificationUtils class

--- app/src/main/java/pl/example/android/garbageapp/utils/NotificationUtils.java
+++ app/src/main/java/pl/example/android/garbageapp/utils/NotificationUtils.java
@@ -1,8 +1,16 @@
 package pl.example.android.garbageapp.utils;
 
+import android.app.Notification;
+
 /**
  * Created by miltomasz on 15/11/17.
  */
 
 public class NotificationUtils {
+
+    private NotificationUtils() {}
+
+    public static Notification popUpNotification() {
+        return null;
+    }
 }
