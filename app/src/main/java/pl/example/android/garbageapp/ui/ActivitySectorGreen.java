9ea973a4ce81b8026545e6ab5c9284bd940bf511
gbielanski <gbielanski@poczta.onet.pl>
2017-11-16 10:49:32 +0100

    setOnClickListener for switch changed

--- app/src/main/java/pl/example/android/garbageapp/ui/ActivitySectorGreen.java
+++ app/src/main/java/pl/example/android/garbageapp/ui/ActivitySectorGreen.java
@@ -1,19 +1,14 @@
 package pl.example.android.garbageapp.ui;
 
-import android.app.Activity;
-import android.arch.lifecycle.ViewModelProviders;
 import android.databinding.DataBindingUtil;
 import android.support.v4.app.FragmentActivity;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.view.View;
-import android.widget.Toast;
 
 import pl.example.android.garbageapp.R;
 import pl.example.android.garbageapp.data.database.SectorType;
 import pl.example.android.garbageapp.databinding.ActivitySectorGreenBinding;
-import pl.example.android.garbageapp.utilities.InjectorUtils;
 
 import static android.support.v7.widget.LinearLayoutManager.VERTICAL;
 
@@ -37,14 +32,11 @@ public class ActivitySectorGreen extends BaseActivitySector {
         LinearLayoutManager verticalLinearLayoutManager = new LinearLayoutManager(this, VERTICAL, false);
         binding.rcSectorTerms.setLayoutManager(verticalLinearLayoutManager);
         binding.rcSectorTerms.setAdapter(sectorTermsAdapter);
+        binding.notificationSwitch.setOnClickListener(this::markForNotification);
 
-        if(checkIfSectorMarkedAsNotification())
+        if(isMarkedForNotification())
             binding.notificationSwitch.setChecked(true);
 
         //TODO mViewModel observe and bind data to UI
-    }
-
-    public void setNotification(View view) {
-        super.setNotification(view);
     }
 }
