32dc94a30ac3816ed8c20a425503b24683038f38
gbielanski <gbielanski@poczta.onet.pl>
2017-11-19 23:28:05 +0100

    bind data from repository to recycler view

--- app/src/main/java/pl/example/android/garbageapp/ui/DetailViewModelFactory.java
+++ app/src/main/java/pl/example/android/garbageapp/ui/DetailViewModelFactory.java
@@ -2,21 +2,24 @@ package pl.example.android.garbageapp.ui;
 
 import android.arch.lifecycle.ViewModel;
 import android.arch.lifecycle.ViewModelProvider;
+import android.support.annotation.NonNull;
 
+import pl.example.android.garbageapp.data.SectorTermRepository;
 import pl.example.android.garbageapp.data.database.SectorType;
 
 public class DetailViewModelFactory  extends ViewModelProvider.NewInstanceFactory {
-    //private final SectorTermRepository mRepository;
+    private final SectorTermRepository mRepository;
     private final SectorType mSectorType;
 
-    public DetailViewModelFactory(/* SectorTermRepository repository,*/ SectorType sectorType) {
-        //this.mRepository = repository;
+    public DetailViewModelFactory(SectorTermRepository repository, SectorType sectorType) {
+        this.mRepository = repository;
         this.mSectorType = sectorType;
     }
 
+    @NonNull
     @Override
-    public <T extends ViewModel> T create(Class<T> modelClass) {
+    public <T extends ViewModel> T create(@NonNull Class<T> modelClass) {
         //noinspection unchecked
-        return (T) new DetailActivityViewModel(/* mRepository,*/ mSectorType);
+        return (T) new DetailActivityViewModel( mRepository, mSectorType);
     }
 }
