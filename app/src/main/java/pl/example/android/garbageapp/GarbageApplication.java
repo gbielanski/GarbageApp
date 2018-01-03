6b2091a35ac36ed9a56854affc521a1a659d06b0
gbielanski <gbielanski@poczta.onet.pl>
2018-01-03 22:33:00 +0100

    Use Stetho

---
+++ app/src/main/java/pl/example/android/garbageapp/GarbageApplication.java
@@ -0,0 +1,16 @@
+package pl.example.android.garbageapp;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Created by gbielanski on 1/3/2018.
+ */
+
+public class GarbageApplication extends Application {
+    public void onCreate() {
+        super.onCreate();
+        Stetho.initializeWithDefaults(this);
+    }
+}
