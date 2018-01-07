6563caefc278e8dfe17286ddb9e6f3d0cb005ca4
Grzegorz Bielanski <Grzegorz.Bielanski@IGT.com>
2018-01-07 16:42:50 +0100

    AlarmReceiver LOG_TAG and AndroidManifest formatting.

--- app/src/main/java/pl/example/android/garbageapp/AlarmReceiver.java
+++ app/src/main/java/pl/example/android/garbageapp/AlarmReceiver.java
@@ -8,14 +8,15 @@ import android.util.Log;
 import pl.example.android.garbageapp.utils.NotificationUtils;
 
 public class AlarmReceiver extends BroadcastReceiver {
+    private static final String LOG_TAG = AlarmReceiver.class.getSimpleName();
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.d("AlarmReceiver", "onReceive");
+        Log.d(LOG_TAG, "onReceive");
         String action = intent.getAction();
         if(action != null){
             if(action.equals("android.intent.action.BOOT_COMPLETED") ||
                     action.equals("android.intent.action.QUICKBOOT_POWERON")){
-                Log.d("AlarmReceiver", action);
+                Log.d(LOG_TAG, action);
                 NotificationUtils.scheduleSectorTermsNotification(context);
             }
 
